d7317bf53fab51edddc402902d77b71183329c03
iroxmega <kirkin.danil2015@yandex.ru>
2024-02-21 05:31:16 +0400

    - useContext removed

--- src/App.js
+++ src/App.js
@@ -2,32 +2,25 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import './App.css';
 import './scss/app.scss';
+
 import Header from "./components/Header";
 import NotFound from "./components/NotFoundBlock";
+
 import Main from "./pages/Main";
-
 import Cart from "./pages/Cart";
-import {useState} from "react";
-
-export const SearchContext = React.createContext('')
 
 function App() {
-    const [searchValue, setSearchValue] = useState('')
-
 
     return (
         <div className="wrapper">
-            <SearchContext.Provider value={{ searchValue, setSearchValue }} >
-                <Header />
-                <div className="content">
-                    <Routes>
-                        <Route path='/' element={<Main/>}/>
-                        <Route path='/cart' element={<Cart/>}/>
-                        <Route path='*' element={<NotFound/>}/>
-                    </Routes>
-
-                </div>
-            </SearchContext.Provider>
+            <Header/>
+            <div className="content">
+                <Routes>
+                    <Route path='/' element={<Main/>}/>
+                    <Route path='/cart' element={<Cart/>}/>
+                    <Route path='*' element={<NotFound/>}/>
+                </Routes>
+            </div>
         </div>
     );
 }
