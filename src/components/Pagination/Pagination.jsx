6990a08c3cebaaffcee15ac9011af96d4730ea29
iroxmega <kirkin.danil2015@yandex.ru>
2024-02-21 05:35:41 +0400

    - page changers logic was carried to Pagination component itself

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from './Pagination.module.scss'
+import {useDispatch, useSelector} from "react-redux";
+import {setCurrentPage} from "../../redux/slices/filterSlice";
 
-const Pagination = (props) => {
-    const {nav = null, disable, onNextPageClick, onPrevPageClick} = props;
+const Pagination = ({items, itemsCount, rowsPerPage} ) => {
+
+    //redux states
+    const currentPage = useSelector((state) => state.filterSlice.currentPage)
+    const dispatch = useDispatch()
+
+    const [pagesCount, setPagesCount] = useState(1)
+
+    useEffect(() => {
+        setPagesCount(Math.ceil(itemsCount / rowsPerPage))
+    }, [itemsCount]);
+
+    //pag elements
+    const disable = { left: currentPage === 1, right: currentPage === pagesCount,}
+    const nav = {current: currentPage, total: pagesCount}
 
     const handleNextPageClick = () => {
-        onNextPageClick();
-    };
+        const next = currentPage + 1;
+        const total = items.length !== 0 ? pagesCount : currentPage;
+        onChangePage(next <= total ? next : currentPage);
+    }
+
     const handlePrevPageClick = () => {
-        onPrevPageClick();
-    };
+        const prev = currentPage - 1;
+        onChangePage(prev > 0 ? prev : currentPage);
+    }
+
+    const onChangePage = num => {
+        console.log(currentPage)
+        dispatch(setCurrentPage(num))
+    }
 
     return (
         <div className={styles.paginator}>
