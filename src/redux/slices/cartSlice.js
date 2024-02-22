72446aab98d9ab7ff4919d2caf920a45e8764ab5
iroxmega <kirkin.danil2015@yandex.ru>
2024-02-22 22:15:27 +0400

    - cart logic by redux toolkit has been added

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,68 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    totalPrice: 0,
+    items: []
+}
+
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            const findItem = state.items.find(pizza =>
+                pizza.id === action.payload.id &&
+                pizza.typeProp === action.payload.typeProp &&
+                pizza.pizzaSize === action.payload.pizzaSize
+            )
+
+            if (findItem) {
+                findItem.count++
+            } else {
+                state.items.push({...action.payload, count: 1})
+            }
+
+            state.totalPrice = state.items.reduce((sum, pizza) => pizza.price * pizza.count + sum, 0)
+
+        },
+        removeItem: (state, action) => {
+            console.log(action.payload)
+            state.items = state.items.filter(item => {
+
+                if (item.id !== action.payload.id) {
+                    return true
+                }
+                else if (item.typeProp !== action.payload.typeProp || item.pizzaSize !== action.payload.pizzaSize) {
+                    return true
+                }
+
+                return false
+            }
+
+            )
+            state.totalPrice = state.items.reduce((sum, pizza) => pizza.price * pizza.count + sum, 0)
+        },
+        clearItems: (state, action) => {
+            state.items = []
+            state.totalPrice = 0
+        },
+
+        decrement: (state, action) => {
+            const findItem = state.items.find(pizza =>
+                pizza.id === action.payload.id &&
+                pizza.typeProp === action.payload.typeProp &&
+                pizza.pizzaSize === action.payload.pizzaSize
+            )
+
+            if (findItem) {
+                findItem.count--
+                state.totalPrice = state.items.reduce((sum, pizza) => pizza.price * pizza.count + sum, 0)
+
+            }
+        }
+    },
+})
+
+export const {addItem, removeItem, decrement, clearItems} = cartSlice.actions
+export default cartSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterSliceReducer from "./slices/filterSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 export const store = configureStore({
-    reducer: { filterSlice: filterSliceReducer },
+    reducer: {
+        filterSlice: filterSliceReducer,
+        cartSlice: cartSliceReducer
+
+    },
 })
